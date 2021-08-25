15143334c94e903eca88656cbfcbd9c52e31d35b
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2021-08-25 23:19:34 +0530

    Added header component - Added boilerplate HTML for amazon header logo, search input and header options

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+	return (
+		<div className='header'>
+			<img
+				alt='amazon_logo'
+				className='header__logo'
+				src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+			/>
+			<div className='header__search'>
+				<input className='header__searchInput' type='text' />
+				{/* Logo */}
+			</div>
+			<div className='header__nav'>
+				<div className='header__option'>
+					<span className='header__option_lineOne'>Hello Guest</span>
+					<span className='header__option_lineTwo'>Sign In</span>
+				</div>
+				<div className='header__option'>
+					<span className='header__option_lineOne'>Returns</span>
+					<span className='header__option_lineTwo'>& Orders</span>
+				</div>
+				<div className='header__option'>
+					<span className='header__option_lineOne'>Your</span>
+					<span className='header__option_lineTwo'>Prime</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
